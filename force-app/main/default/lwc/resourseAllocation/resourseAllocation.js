a245d45271a8e476e72d43ab9f0bd3ff4c2d6320
robinlacruz <robin.lacruz@gmail.com>
2022-05-24 18:02:17 -0300

    componente visual

--- force-app/main/default/lwc/resourseAllocation/resourseAllocation.js
+++ force-app/main/default/lwc/resourseAllocation/resourseAllocation.js
@@ -3,13 +3,25 @@ import getProjects from '@salesforce/apex/ProjectAndResources.getProjects';
 import getResourcesByRole from '@salesforce/apex/ProjectAndResources.getResourcesByRole';
 import getProjectLineItems from '@salesforce/apex/ProjectAndResources.getProjectLineItems';
 import getProjectAssignedResources from '@salesforce/apex/ProjectAndResources.getProjectAssignedResources';
+import getResourcesById from '@salesforce/apex/ProjectAndResources.getResourcesById';
+
+const columns = [
+    { label: 'Resource Name', fieldName: 'resourceName', editable: false },
+    { label: 'Role', fieldName: 'resourceRole', type: 'picklist', editable: false },
+    { label: 'Rate', fieldName: 'resourceRate', type: 'currency', editable: false },
+    { label: 'Start Date', fieldName: 'startDate', type: 'date-local', editable: true },
+    { label: 'End Date', fieldName: 'endDate', type: 'date-local', editable: true },
+];
 
 export default class ResourseAllocation extends LightningElement {
     project;
     resourcesByRole;
     projectLineItems;
     projectId;
-    value=[];
+    columns=columns;
+    rowOffset = 0;
+    resourcesById;
+    projectLineItemsByRole;
 
     
     @wire (getProjects)
@@ -17,7 +29,7 @@ export default class ResourseAllocation extends LightningElement {
         const {data,error} = result;
         this.project=null;
         if(data){
-            if(data.length>0) this.project=data[0];
+            if(data.length>0) this.project = data[0];
             this.projectId=this.project.Id;
             console.log('recibimos projects: ',data);
         } else if(error){
@@ -33,20 +45,24 @@ export default class ResourseAllocation extends LightningElement {
         if(data){
             let receivedPLIs = data;
             let PLIarray=[];
-            console.log('antes del for de projectLineItems',data);
+            //console.log('antes del for de projectLineItems',data);
             for(let i=0;i<receivedPLIs.length;i++){
                 let {Id, Name, Role__c, Estimated_Hours__c}  = receivedPLIs[i];
-                console.log('Id->'+Id+' Name->'+Name+' Role__c->'+Role__c+' Estimated_Hours__c'+Estimated_Hours__c);
+
                 let resources = this.getResource(receivedPLIs[i].Role__c);
-                let checkboxResources = [];
-                checkboxResources = resources.map(element=>{
-                    return {label:element.Name,value:{resourceId:element.Id,PLIId:receivedPLIs[i].Id}}
-                });
                 console.log('Resources->'+JSON.stringify(resources));
-                PLIarray[i] = {Id:Id,Name:Name,Role__c:Role__c,Estimated_Hours__c:Estimated_Hours__c,Resources:checkboxResources};
+                let data1 = [];
+                resources.forEach(element => {
+                    data1.push({resourceId:element.Id,resourceName:element.Name,resourceRate:element.Rate_p_hour__c,startDate:null,endDate:null,pliId:receivedPLIs[i].Id,resourceRole:element.Role__c});
+                });
+
+                PLIarray[i] = {Id:Id,Name:Name,Role__c:Role__c,Estimated_Hours__c:Estimated_Hours__c,data1:data1};
             }
             this.projectLineItems=PLIarray;
-            console.log('projectLineItems: ',this.projectLineItems);
+            this.projectLineItemsByRole= new Map(this.projectLineItems.map(elemento=>{
+                return [elemento.Role__c, elemento];
+            }));
+            console.log('projectLineItems: ',this.projectLineItems); 
             
         } else if(error){
             console.log('Hubo error recibiendo projectLineItems', error);
@@ -58,7 +74,20 @@ export default class ResourseAllocation extends LightningElement {
         const {data,error} = result;
         if(data){
             this.resourcesByRole=data;
-            console.log('recibimos resourcesByRole: ',data);
+            //console.log('recibimos resourcesByRole: ',data);
+
+        } else if(error){
+            console.log('Hubo error recibiendo resourcesByRole', error);
+        }
+    }
+
+    @wire (getResourcesById)
+    receivedRescs(result){
+        const {data,error} = result;
+        if(data){
+            this.resourcesById=data;
+            //console.log('recibimos resourcesById: ',data);
+
         } else if(error){
             console.log('Hubo error recibiendo resourcesByRole', error);
         }
@@ -69,12 +98,43 @@ export default class ResourseAllocation extends LightningElement {
     }
     
     get selectedValues() {
-        return this.value?this.value.resourceId/* .join(',') */:'ninguno';
+        return 'Por ahora nada';
     }
 
-    handleChange(e) {
-        this.value = e.detail.value;
-        console.log('evento en handleChange')
-        console.log(JSON.parse(JSON.stringify(e.detail.value)));
+    handleSave(data) {
+        
+        //console.log('los draftValues son:');
+        //console.log(JSON.parse(JSON.stringify(data.detail.draftValues)));
+        let editedRecords = data.detail.draftValues;
+        let pliToInsert=[];
+        editedRecords.forEach(element => {
+            let user = this.resourcesById[element.resourceId];
+            let pli = this.projectLineItemsByRole.get(user.Role__c);
+            let businessDays = getBusinessDatesCount(new Date(element.startDate), new Date(element.endDate))
+            let assignedHours = 8*businessDays;
+            //console.log('assignedHours -> ',assignedHours);
+            let projAssignResource = {Name:`ProjAssRes${user.Name}`, User__c:user.Id,Start_Date__c:element.startDate,End_Date__c:element.endDate ,Project_Line_Item__c:pli.Id ,Assigned_Hour__c:assignedHours};
+            pliToInsert.push(projAssignResource);
+
+        }); 
+        console.log(pliToInsert); 
+
+        function getBusinessDatesCount(startDate, endDate) {
+            let count = 0;
+            const curDate = new Date(startDate.getTime());
+            while (curDate <= endDate) {
+                const dayOfWeek = curDate.getDay();
+                if(dayOfWeek !== 0 && dayOfWeek !== 6) count++;
+                curDate.setDate(curDate.getDate() + 1);
+            }
+            return count;
+        }
     }
+
+    handleCancel(data) {
+        console.log('los draftValues son:');
+        console.log(JSON.parse(JSON.stringify(data.detail)));
+    }
+
+
 }
