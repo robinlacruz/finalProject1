d41b9f34ddd367ff06649f1d2e283fc56f822175
Nico Linares Mayer <niliyer001@gmail.com>
2022-06-06 16:57:06 -0300

    changes

--- force-app/main/default/lwc/projectTasks/projectTasks.js
+++ force-app/main/default/lwc/projectTasks/projectTasks.js
@@ -1,7 +1,5 @@
 import { LightningElement, api } from "lwc";
 export default class ProjectTasks extends LightningElement {
   @api currentProjectTasks;
-  /* connectedCallback(){
-        this.currentProjectTasks = JSON.parse(JSON.stringify(this.currentProjectTasks));
-    } */
+
 }
